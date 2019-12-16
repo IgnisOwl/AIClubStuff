2fc9d73e3074b56cb53f845b6cf4c228c20868f7
Connor Michalec <35946564+IgnisOwl@users.noreply.github.com>
2019-12-16 11:25:59 -0600

    Update AnimalNeuralNetwork.py

--- AnimalNeuralNetwork.py
+++ AnimalNeuralNetwork.py
@@ -1,5 +1,5 @@
 #NOTE: not configured to run with the GPU on non-google-collab runtime environments
-#NOTE: there is some differences from the slides on here
+#The Animal Classification Neural Network
 
 import numpy as np
 import pandas as pd
@@ -34,7 +34,7 @@ model = keras.models.Sequential() #type of network
 columnAmount = trainX.shape[1] #get the amount of columns in the training Input
 
 model.add(keras.layers.Dense(16, activation="relu", input_shape=(columnAmount,))) #input layer, has 17 inputs, aka the amount of columns minus the output column(s), input shape specifies the number of rows and columns, first we set the amount of columns, then after we put nothing because we can have any amount of rows
-model.add(keras.layers.Dense(32, activation="relu")) #hidden layer(32)
+model.add(keras.layers.Dense(32, activation="relu")) #hidden layer(32 neurons)
 model.add(keras.layers.Dense(1)) #the output is the class_type that it guesses
 
 #now we must compile the model:
@@ -44,14 +44,16 @@ model.compile(optimizer="adam", loss="mean_squared_error")
 
 #now we gotta actually train the model
 
-model.fit(trainX, trainY, epochs=500) #trainX is the training inputs, trainY is the training outputs, remember, epochs is the amount of times it tries all of the data in the csv and backpropigates
+model.fit(trainX, trainY, epochs=300) #trainX is the training inputs, trainY is the training outputs, remember, epochs is the amount of times it tries all of the data in the csv and backpropigates
 
 
 #Now let's test:
 
 
 testY = model.predict(testX)
-#this starts at index 92 on the actual github sheet if ur looking
-print(testY) #print predicted outputs
-
+#this starts at index 92 on the actual github sheet if ur looking, because of the list thing where it starts at zero and the fact that it has all the column names at top
+print("TEST Y:")
+print(testY)
+print("ROUNDED TEST Y:")
+print(testY.round()) #print predicted outputs(rounded)
 
